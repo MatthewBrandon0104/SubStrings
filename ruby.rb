a684f4f3ac9113ca6e10b93fba68ea81a94aee57
Matthew Brandon <matthewbrandon0104@gmail.com>
2018-07-22 17:56:31 -0700

    basic functions, needs to drop zeros

---
+++ ruby.rb
@@ -0,0 +1,25 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substring(string, dictionary)
+    counts = {}
+    array = string.downcase.split(" ")
+    
+    dictionary.each do |count_key|
+        counts[count_key] = 0
+    end
+    
+    array.each do |word|
+        dictionary.each do |dic_word|
+            if word.include? dic_word
+                counts[dic_word] += 1
+            end
+        end
+    end
+    puts counts
+end
+
+substring("below", dictionary)
+
+
+
+# "Howdy partner, sit down! How's it going?"
