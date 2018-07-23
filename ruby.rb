aa148057ce483223810c266ba0936debe01461ac
Matthew Brandon <matthewbrandon0104@gmail.com>
2018-07-22 18:22:28 -0700

    refactored

--- ruby.rb
+++ ruby.rb
@@ -3,23 +3,9 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 def substring(string, dictionary)
     counts = {}
     array = string.downcase.split(" ")
-    
-    dictionary.each do |count_key|
-       counts[count_key] = 0
-    end
-    
-    array.each do |word|
-        dictionary.each do |dic_word|
-            if word.include? dic_word
-                counts[dic_word] += 1
-            end
-        end
-    end
+    dictionary.each {|count_key| counts[count_key] = 0 }
+    array.each {|word| dictionary.each {|dic_word| counts[dic_word] += 1 if word.include? dic_word }}
     puts counts.select {|key, value| value > 0}
 end
 
 substring("Howdy partner, sit down! How's it going?", dictionary)
-
-
-
-# "Howdy partner, sit down! How's it going?"
